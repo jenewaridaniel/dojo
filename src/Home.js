65ebea9c05074e411fb9fddbc4b7725bffd8bbf1
Daniel <jenwaridaniel123@gmail.com>
2022-10-22 01:48:34 +0100

    changed

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 const Home = () => {
+    let[name,Setname] =useState('itachi') 
+    let[person,setPerson]=useState('good')
+
+    let Handler =()=>{
+        Setname('obito')
+        setPerson('Negs')
+    }
     return (  
+      
         <div className="home">
             <h1>Homepage</h1>
+            <p>{name} is {person}</p>
+            <button onClick={Handler}>click here</button>
         </div>
     );
 }
